a5268a8026b8c5c6e746dcc84fe33fdabb06cf26
destineddave@gmail.com <info@mainlogix.io>
2024-07-31 10:53:24 +0100

    sensor_values declared

---
+++ lib/sensor-read/src/sensor_values.hpp
@@ -0,0 +1,64 @@
+#ifndef __SENSOR_VALUES_H__
+#define __SENSOR_VALUES_H__
+
+namespace SensorRead
+{
+    
+    class SensorReadingInterface
+    {
+    public:
+    
+        SensorReadingInterface(int threshold, int pinNumber);
+
+        // Pure virtual functions
+        virtual int readSensor() = 0;
+        virtual bool getState() = 0;
+
+    protected:
+        int threshold;
+        int pinNumber;
+    };
+
+
+    struct SensorPinConfig
+    {
+        int solarPin;
+        int batteryPin;
+        int peakAm;
+        int peakPm;
+    };
+
+
+    class LDR : public SensorReadingInterface
+    {
+    public:
+        LDR(int threshold, int pinNumber);
+        int readSensor() override;
+        bool getState() override;
+    };
+
+
+    class Battery : public SensorReadingInterface
+    {
+    public:
+        Battery(int threshold, int pinNumber);
+        int readSensor() override;
+        bool getState() override;
+    };
+
+
+    class Grid : public SensorReadingInterface
+    {
+    public:
+        Grid(int pinPeakAm, int pinPeakPm);
+        int readSensor() override;
+        bool getState() override;
+
+    private:
+        int pinPeakAm;
+        int pinPeakPm;
+    };
+}
+
+#endif 
+
