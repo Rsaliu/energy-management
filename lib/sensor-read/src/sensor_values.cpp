dc9d8eb1e1e9f050d5269cea8bc4b2a623ee6c0f
destineddave@gmail.com <info@mainlogix.io>
2024-07-31 10:52:05 +0100

    sensor_value defined

---
+++ lib/sensor-read/src/sensor_values.cpp
@@ -0,0 +1,52 @@
+#include "sensor_values.hpp"
+#include <Arduino.h>
+
+using namespace SensorRead;
+
+// SensorReadingInterface constructor
+SensorReadingInterface::SensorReadingInterface(int threshold, int pinNumber)
+    : threshold(threshold), pinNumber(pinNumber) {}
+
+// LDR class
+LDR::LDR(int threshold, int pinNumber) 
+    : SensorReadingInterface(threshold, pinNumber) {}
+
+int LDR::readSensor()
+{
+    return analogRead(pinNumber);
+}
+
+bool LDR::getState()
+{
+    return readSensor() >= threshold;
+}
+
+// Battery class
+Battery::Battery(int threshold, int pinNumber) 
+    : SensorReadingInterface(threshold, pinNumber) {}
+
+int Battery::readSensor()
+{
+    return analogRead(pinNumber);
+}
+
+bool Battery::getState()
+{
+    return readSensor() >= threshold;
+}
+
+// Grid class
+Grid::Grid(int pinPeakAm, int pinPeakPm) 
+    : SensorReadingInterface(0, 0), pinPeakAm(pinPeakAm), pinPeakPm(pinPeakPm) {}
+
+int Grid::readSensor()
+{
+    if (digitalRead(pinPeakPm) == HIGH) return 1;
+    if (digitalRead(pinPeakAm) == HIGH) return 2;
+    return 0;
+}
+
+bool Grid::getState()
+{
+    return readSensor() != 0;
+}
